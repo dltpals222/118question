9262efa9181eeedc0891e423f79361a0a4ad0fcb
leesemin <leesemin222@gmail.com>
2023-07-20 12:44:50 +0900

    홀짝 py

---
+++ Python/EvenOdd.py
@@ -0,0 +1,17 @@
+def EvenOdd(a:int):
+    if a % 2 == 1:
+        print(a,"는 홀수입니다.")
+    else: 
+      if a % 2 == 0:
+        print(a,"는 짝수입니다.")
+
+def EvenOddTwo(a:int):
+    if a % 2 == 1:
+        print(a,"는 홀수입니다.")
+    else: 
+        print(a,"는 짝수입니다.")
+
+EvenOdd(1)
+EvenOdd(2)
+EvenOddTwo(1)
+EvenOddTwo(2)

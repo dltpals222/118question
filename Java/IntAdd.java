7e03c137868122895f1206d67a38a97cd9539332
leesemin <leesemin222@gmail.com>
2023-07-20 12:15:07 +0900

    함수 : 더하기 java

---
+++ Java/IntAdd.java
@@ -0,0 +1,10 @@
+public class IntAdd {
+  static int add (int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    int result = add(5,10);
+    System.out.print(result);
+  }
+}

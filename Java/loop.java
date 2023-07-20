90d1822bf31b337365d459528574ccb32f73be13
leesemin <leesemin222@gmail.com>
2023-07-20 12:31:11 +0900

    루프문 0~9, 1~10 java

---
+++ Java/loop.java
@@ -0,0 +1,17 @@
+public class loop {
+  public static void zeroNine(){
+    for(int i = 0 ; i < 10 ; i++){
+      System.out.print(i);
+    }
+  }
+
+  public static void oneTen(){
+    for(int i = 1 ; i <= 10 ; i++){
+      System.out.print(i);
+    }
+  }
+  public static void main (String[] args) {
+    zeroNine();
+    oneTen();
+  }
+}

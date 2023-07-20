8436974f5301a3173eee0c3ec02a14a1ba89ff8e
leesemin <leesemin222@gmail.com>
2023-07-20 10:03:51 +0900

    3개 폴더 안에 파일을 만드는 로직 생성

--- index.py
+++
@@ -1,6 +0,0 @@
-def add (a : int, b: int ) -> int:
-    return a + b
-
-addResult = add(5,10)
-
-print(addResult)
---
+++ makeFile.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const fileName = "imsi";
+
+const list = [".java", ".js", ".py"];
+const dirList = ["Java", "JavaScript", "Python"];
+const mkFile = list.map((item, i) => path.join(__dirname, dirList[i], fileName + item));
+
+mkFile.map((item) => fs.writeFileSync(item, ""));

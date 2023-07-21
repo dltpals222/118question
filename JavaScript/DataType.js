6ba8d96149ba75178a66168533147ec9b3d602b3
leesemin <leesemin222@gmail.com>
2023-07-21 17:55:36 +0900

    타입확인 js

---
+++ JavaScript/DataType.js
@@ -0,0 +1,6 @@
+const typeString = typeof "string";
+const typeNumber = typeof 123;
+const typeBoolean = typeof true;
+const typeObject = typeof [];
+
+console.log(typeString, typeNumber, typeBoolean, typeObject);

f238deccd9e96050134fc84d90d89c2fd6788fce
leesemin <leesemin222@gmail.com>
2023-07-21 18:52:12 +0900

    변수에 할당된 값이 일치하는지 확인 java

---
+++ Java/DataCheck.java
@@ -0,0 +1,10 @@
+public class DataCheck {
+  public static void main(String[] args){
+    int x = 10;
+    if(x == 10){
+      System.out.println("변수 x는 10의 값을 갖고있습니다.");
+    } else {
+      System.out.println("변수 x는 10이 아닌 값을 갖고있습니다.");
+    }
+  }
+}

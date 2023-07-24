de26da6e52346da6dedfe375ffaacf88ee146d84
leesemin <leesemin222@gmail.com>
2023-07-24 20:20:19 +0900

    사칙연산 py

---
+++ Python/Arithmetic.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    return a + b
+def minus(a,b):
+    return a - b
+def multi(a,b):
+    return a * b
+def division(a,b):
+    return a / b
+
+a = 4
+b = 2 
+
+testAdd = add(a,b)
+testMinus = minus(a,b)
+testMulti = multi(a,b)
+testDivision = division(a,b)
+
+print(testAdd)
+print(testMinus)
+print(testMulti)
+print(testDivision)

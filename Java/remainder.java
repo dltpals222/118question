4ebdcf9c3232351c798768c82e12b543405a9b50
leesemin <leesemin222@gmail.com>
2023-07-25 04:46:32 +0900

    나머지 구하기 java

---
+++ Java/remainder.java
@@ -0,0 +1,11 @@
+public class remainder {
+  public static int testRemainder (int a, int b) {
+    return a % b;
+  }
+
+  public static void main (String[] A) {
+    int testOne = testRemainder(10, 3);
+    
+    System.out.print(testOne);
+  }
+}

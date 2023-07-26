be759332f0cedb03b3b6a0df5151e7809d21bda8
leesemin <leesemin222@gmail.com>
2023-07-27 06:07:54 +0900

    몫 구하기 java

---
+++ Java/Quotient.java
@@ -0,0 +1,10 @@
+public class Quotient {
+  public static int quotient (int a, int b) {
+    return a / b;
+  }
+
+  public static void main (String[] args) {
+    int result = quotient(10,2);
+    System.out.println(result);
+  }
+}

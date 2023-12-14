2d6198db91423eef7c9b43ea2f59bc091ebc265c
blahblah <leesemin222@gmail.com>
2023-12-14 16:44:30 +0900

    commander로 script에서 파일만들기 생성 완료

--- makeFile.js
+++ makeFile.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-const fileName = "Conditional";
+function makeFile(fileName) {
+  const list = [".java", ".ts", ".py"];
+  const dirList = ["Java", "JavaScript", "Python"];
+  const mkFile = list.map((item, i) => path.join(__dirname, dirList[i], fileName + item));
+  mkFile.map((item) => fs.writeFileSync(item, ""));
+}
 
-const list = [".java", ".ts", ".py"];
-const dirList = ["Java", "JavaScript", "Python"];
-const mkFile = list.map((item, i) => path.join(__dirname, dirList[i], fileName + item));
-
-mkFile.map((item) => fs.writeFileSync(item, ""));
+module.exports = makeFile;
--- start.js
+++ start.js
@@ -1,4 +1,13 @@
-const program = require('commander')
+const program = require('commander');
+const makeFile = require('./makeFile')
 
 program
-  .commend('name')
+  .arguments('<filename>')
+  // .option('-t, --title <title>', 'set title')
+  // .option('-a, --author <author>', 'set author')
+  .action((filename) => {
+    makeFile(filename)
+  });
+
+
+  program.parse(process.argv);
